2805e3b560543cf8b98524262cd555a3f1fc1da4
avcjeewantha <avcjeewantha.16@cse.mrt.ac.lk>
2020-04-27 01:14:40 +0530

    Enable log-In to autherized users

--- lib/authservices.dart
+++ lib/authservices.dart
@@ -1,9 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:smart_bin/driver_map.dart';
+import 'package:smart_bin/searchservice.dart';
 import 'package:smart_bin/utils/flushbar.dart';
 
 class AuthService {
+  static FirebaseUser okUser;
 
   signOut() {
     FirebaseAuth.instance.signOut();
@@ -11,23 +14,35 @@ class AuthService {
 
   signIn(AuthCredential authCreds, context) {
     FirebaseAuth _auth = FirebaseAuth.instance;
-    _auth.signInWithCredential(authCreds).then((value) => {
-      _auth.currentUser().then((user) {
-        if (user != null) {
-          Navigator.of(context).pop();
-          Navigator.push(context,
-              new MaterialPageRoute(builder: (context) => new DriverMap()));
-        } else {
-          Navigator.of(context).pop();
-        }
-      })
-    }).catchError((onError){
-    if (onError.code.contains('ERROR_NETWORK_REQUEST_FAILED'))
-      ShowFlushbar.showMessage('Network Error', context);
-    else
-      ShowFlushbar.showMessage('Error', context);
+    _auth
+        .signInWithCredential(authCreds)
+        .then((value) => {
+              _auth.currentUser().then((user) {
+                if (user != null) {
+                  SearchService()
+                      .searchPhoneNo(user.phoneNumber)
+                      .then((QuerySnapshot docs) {
+                    if (docs.documents.length != 1) {
+                      ShowFlushbar.showMessage('Not allowed to login', context);
+                    } else {
+                      Navigator.of(context).pop();
+                      Navigator.push(
+                          context,
+                          new MaterialPageRoute(
+                              builder: (context) => new DriverMap()));
+                    }
+                  });
+                } else {
+                  Navigator.of(context).pop();
+                }
+              })
+            })
+        .catchError((onError) {
+      if (onError.code.contains('ERROR_NETWORK_REQUEST_FAILED'))
+        ShowFlushbar.showMessage('Network Error', context);
+      else
+        ShowFlushbar.showMessage('Error', context);
     });
-    
   }
 
   signInWithOTP(smsCode, verId, context) {
@@ -38,6 +53,13 @@ class AuthService {
 
   Future<FirebaseUser> getCurrentUser() async {
     FirebaseUser user = await FirebaseAuth.instance.currentUser();
-    return user;
-  }
+    SearchService().searchPhoneNo(user.phoneNumber).then((QuerySnapshot docs) {
+      if (docs.documents.length == 1) {
+        print(docs.documents);
+        okUser = user;
+      }
+    });
+    return okUser;
+    }
+
 }
