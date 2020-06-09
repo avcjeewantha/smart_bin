6cd8ff937885920117c37203d9009a2e0e92dfa7
sandunK <gmgsk.16@cse.mrt.ac.lk>
2020-06-09 21:04:10 +0530

    Server application added

---
+++ smartbin_server/dataSave.js
@@ -0,0 +1,34 @@
+const admin = require('firebase-admin');
+let http = require('http');
+
+let serviceAccount = require('./smart-bin-273112-6339b0df47cc.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+let db = admin.firestore();
+
+/**
+ * Created by Sandun Gunasekara on 06/08/20.
+ * Function to save data into firebase
+ * @type {{saveData: saveData}}
+ */
+
+module.exports = {
+    saveData: function (binId,binStatus,successCallback,errorCallback) {
+        console.info("Connecting into firebase ...");
+        try {
+            let docRef = db.collection('Bin').doc(binId);
+
+            let setAda = docRef.set({
+                status: binStatus;
+            });
+            console.info("Bin status: "+binStatus+" saved successfully for bin id: "+binId+"...");
+            successCallback("Bin status: "+binStatus+" saved successfully for bin id: "+binId+"...");
+        } catch (e) {
+            errorCallback(e);
+        }
+
+    }
+}
